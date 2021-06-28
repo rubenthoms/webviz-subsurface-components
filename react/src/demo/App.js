f61f53a8c9571032e25804e62a4c17e94483b5bb
Jørgen Herje <jorgen.herje@ceetronsolutions.com>
2021-06-28 10:57:08 +0200

    Add vector calculator component
    - Implementation of Vector Calculator react component 
    - Demo application for test

--- react/src/demo/App.js
+++ react/src/demo/App.js
@@ -18,11 +18,12 @@ import SubsurfaceMapDemo from "./SubsurfaceMapDemo";
 import WellCompletionsDemo from "./WellCompletionsDemo";
 import VectorSelectorDemo from "./VectorSelectorDemo";
 import WellLogViewerDemo from "./WellLogViewerDemo";
+import VectorCalculatorDemo from "./VectorCalculatorDemo";
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { value: "DeckGLMapDemo" };
+        this.state = { value: "VectorCalculator" };
     }
 
     onChange(e) {
@@ -31,6 +32,9 @@ class App extends Component {
 
     renderDemo() {
         switch (this.state.value) {
+            case "VectorCalculator": {
+                return <VectorCalculatorDemo />;
+            }
             case "DeckGLMapDemo": {
                 return <DeckGLMapDemo />;
             }
@@ -77,6 +81,7 @@ class App extends Component {
                     value={this.state.value}
                     onChange={this.onChange.bind(this)}
                 >
+                    <option value="VectorCalculator">VectorCalculator</option>
                     <option value="DeckGLMapDemo">DeckGLMapDemo</option>
                     <option value="GroupTree">GroupTree</option>
                     <option value="LeafletMap">LeafletMap</option>
